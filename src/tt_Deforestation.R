e5e4e0496bfe421968dc989a2a44062e4df174df
josephedwardoreilly <jo13931@bristol.ac.uk>
2021-04-08 17:59:03 +0100

    Final version of the plot and clean-er code

--- src/tt_Deforestation.R
+++ src/tt_Deforestation.R
@@ -1,51 +1,54 @@
-require(tidytuesdayR)
-require(ggplot2)
-require(data.table)
-require(ggtext)
-require(geofacet)
+library(tidytuesdayR)
+library(ggplot2)
+library(data.table)
+library(ggtext)
+library(geofacet)
 library(patchwork)
 
 
+# Data Prep ---------------------------------------------------------------
+# Read the data
 tidy.week <- '2021-04-06'
 tt_data <- tt_load(tidy.week) 
-
 x <- data.table(tt_data$forest_area)
 
-
-
+# obtain a tile structure for countries of the world
 wt <- data.table(geofacet::world_countries_grid1)
 
-
-# get the avergae covergae in last decade 
+# get the average coverage in last decade 
 x <- x[year %in% 2010:2020,
        .(forest_area = mean(forest_area, na.rm = TRUE)), by = .(code)]
-
-
+# Merge the tile information onto the forest data
 z <- merge(x, 
       wt, 
       by.x = 'code',
       by.y = 'code_alpha3',
       all.y = TRUE)
 
-
+# A colour palette going from yellow to green
 pal.gen <- colorRampPalette(c("#DBDFAC", "#114A50"))
 pal <- setNames(pal.gen(6), as.character(1:6))
 
-bins <- c(0, 0.001, 0.01, 0.1,
-          1, 10, 100)
-z[, bin := as.character(.bincode(
-  forest_area,
-  bins,
-  include.lowest = TRUE))] # include forest_area == 0 in lowest bin
-
-z[code == 'ATA', bin := '1'] # Antartica has no forest
-
-
+# Background colour
 bg = '#FBFFF1'
 
-p <- ggplot(z, aes(x = col, y = -row, fill = bin))+
+# Bin the forest coverage by percentage
+bins <- c(0, 0.001, 0.01, 0.1,
+          1, 10, 100)
+z[, bin := as.character(
+  .bincode(forest_area, bins, include.lowest = TRUE))] # forest_area == 0 in lowest bin
+
+z[code == 'ATA', bin := '1'] # Antarctica has no forest
+
+
+
+
+# Plotting ----------------------------------------------------------------
+
+# Main tile plot
+p <- ggplot(z, aes(x = col, y = -row, fill = bin)) +
   geom_tile(color = 'grey10') +
-  coord_equal() +
+  coord_equal(expand = FALSE) +
   geom_text(
     aes(x = col, y = -row, label = code, color = bin),
     size = 3, family = 'Bebas') +
@@ -57,7 +60,7 @@ p <- ggplot(z, aes(x = col, y = -row, fill = bin))+
         values = rep('black', 6),
         na.value = 'grey10') +
   guides(color = FALSE,
-         fill = FALSE)+
+         fill = FALSE) +
   # Text body
   geom_textbox(
     data = data.frame(
@@ -65,37 +68,39 @@ p <- ggplot(z, aes(x = col, y = -row, fill = bin))+
       y = -21,
       label = "**Distribution Of Total Global Forest Coverage (2010-2020)**<br> 
       <span style = 'font-size:10pt'> Each tile represents a single nation or administrative entity and is shaded according to the average percentage of the global forest coverage found within that area in the last decade.</span>"),
-  aes(x, y, label = label), inherit.aes = FALSE,
-  color = 'black',
-  family = 'Raleway',
-  box.color = NA,
-  fill = NA,
-  size = 5,
-  halign = 0.5,
-  width = grid::unit(0.3, "npc"), 
-  height =  grid::unit(0.1, "npc"), 
-  hjust = 0, vjust = 0
-  ) + 
+    aes(x, y, label = label),
+    inherit.aes = FALSE,
+    color = 'black',
+    family = 'Raleway',
+    box.color = NA,
+    fill = NA,
+    size = 5,
+    halign = 0.5,
+    width = grid::unit(0.3, "npc"), 
+    height =  grid::unit(0.1, "npc"), 
+    hjust = 0,
+    vjust = 0) + 
   labs(caption = "Visualisation by Joe O'Reilly (github.com/josephedwardoreilly)<br>data from tidyTuesday and Our World In Data") +
   theme(
     text = element_text('Raleway'),
     panel.background = element_rect(color = bg, fill = bg),
     panel.border = element_blank(),
     plot.background = element_rect(fill = bg, color = bg),
-    plot.margin = margin(c(10, 5 , 10, 10)),
+    plot.margin = margin(c(10, 20 , 10, 20)),
     plot.caption = element_markdown(family = 'Raleway', colour = 'grey50'),
     plot.caption.position = 'plot')
 
 # Legend 
-legend <- data.table(level = c(NA, sort(z[,unique(bin)])),
-                 xmin = c(0:6),
-                 xmax = c(1:7), 
-                 label = c('Missing', '<0.001%', '0.001 - 0.01%', '0.01 - 0.1%',
-                           '0.1 - 1%', '1 - 10%', '>10%'),
-                 loc = c(-0.5, 1.5, -0.5, 1.5, -0.5, 1.5, -0.5))
+legend <- data.table(
+  level = c(NA, sort(z[,unique(bin)])),
+  xmin = c(0:6),
+  xmax = c(1:7), 
+  label = c('Missing', '<0.001%', '0.001 - 0.01%', '0.01 - 0.1%',
+            '0.1 - 1%', '1 - 10%', '>10%'),
+  loc = c(-0.5, 1.5, -0.5, 1.5, -0.5, 1.5, -0.5))
 
 
-leg <- ggplot(legend) +
+p.leg <- ggplot(legend) +
   geom_rect(
     aes(xmin = xmin, xmax = xmax, ymin = 0, ymax = 1, fill = level), 
     color = 'grey10',
@@ -106,18 +111,18 @@ leg <- ggplot(legend) +
     hjust = 'middle',
     size = 3,
     colour = "black",
-    family = "Bebas")+
-  scale_fill_manual(values = pal, na.value = '#4B3B40')+
-  scale_y_continuous(limits = c(-2,2))+
+    family = "Bebas") +
+  scale_fill_manual(values = pal, na.value = '#4B3B40') +
+  scale_y_continuous(limits = c(-2,2)) +
   guides(fill = FALSE) +
   theme_void() + 
-  theme(plot.margin = margin(c(2, 2 , 2, 2)))+
+  theme(plot.margin = margin(c(2, 2 , 2, 2))) +
   coord_equal()
 
 
 # build plot 
 p + inset_element(
-  leg,
+  p.leg,
   left = 0.75, 
   bottom = 0.85,
   right = 1,
@@ -126,7 +131,7 @@ p + inset_element(
   align_to = "full") +
   ggsave(
     filename = paste0(getwd(), '/plots/', tidy.week, '.png'),
-    width = 16, height = 10.5, device = 'png')
+    width = 10.5, height = 10.5, device = 'png')
 
 
 
