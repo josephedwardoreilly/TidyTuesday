458980653b9a4ffeea226771eeadc44a369bb76f
josephedwardoreilly <jo13931@bristol.ac.uk>
2021-04-08 15:45:26 +0100

    rendered plot, push of code before a quick tidying up

--- src/tt_Deforestation.R
+++ src/tt_Deforestation.R
@@ -1,6 +1,10 @@
 require(tidytuesdayR)
 require(ggplot2)
 require(data.table)
+require(ggtext)
+require(geofacet)
+library(patchwork)
+
 
 tidy.week <- '2021-04-06'
 tt_data <- tt_load(tidy.week) 
@@ -29,12 +33,17 @@ pal <- setNames(pal.gen(6), as.character(1:6))
 
 bins <- c(0, 0.001, 0.01, 0.1,
           1, 10, 100)
-z[, bin := as.character(.bincode(forest_area, bins))]
+z[, bin := as.character(.bincode(
+  forest_area,
+  bins,
+  include.lowest = TRUE))] # include forest_area == 0 in lowest bin
+
+z[code == 'ATA', bin := '1'] # Antartica has no forest
 
 
-bg = '#BAD7F2'
+bg = '#FBFFF1'
 
-ggplot(z, aes(x = col, y = -row, fill = bin))+
+p <- ggplot(z, aes(x = col, y = -row, fill = bin))+
   geom_tile(color = 'grey10') +
   coord_equal() +
   geom_text(
@@ -48,17 +57,14 @@ ggplot(z, aes(x = col, y = -row, fill = bin))+
         values = rep('black', 6),
         na.value = 'grey10') +
   guides(color = FALSE,
-         fill = guide_legend(
-           title = 'Global Forest Cover',
-           label.position = "top", nrow = 1, ncol = 7,
-           label.hjust = 0.5))+
+         fill = FALSE)+
   # Text body
   geom_textbox(
     data = data.frame(
       x = 0,
       y = -21,
-      label = "**National Share Of Total Global Forest Coverage**<br> 
-      <span style = 'font-size:10pt'> Each square represents a single nation, and is shaded according to the percentage of the global forest coverage found within that country.</span>"),
+      label = "**Distribution Of Total Global Forest Coverage (2010-2020)**<br> 
+      <span style = 'font-size:10pt'> Each tile represents a single nation or administrative entity and is shaded according to the average percentage of the global forest coverage found within that area in the last decade.</span>"),
   aes(x, y, label = label), inherit.aes = FALSE,
   color = 'black',
   family = 'Raleway',
@@ -70,11 +76,61 @@ ggplot(z, aes(x = col, y = -row, fill = bin))+
   height =  grid::unit(0.1, "npc"), 
   hjust = 0, vjust = 0
   ) + 
+  labs(caption = "Visualisation by Joe O'Reilly (github.com/josephedwardoreilly)<br>data from tidyTuesday and Our World In Data") +
   theme(
     text = element_text('Raleway'),
     panel.background = element_rect(color = bg, fill = bg),
     panel.border = element_blank(),
-    legend.position="top",
-    legend.justification='right',
     plot.background = element_rect(fill = bg, color = bg),
-    plot.margin = margin(c(10, 5 , 10, 10)))
+    plot.margin = margin(c(10, 5 , 10, 10)),
+    plot.caption = element_markdown(family = 'Raleway', colour = 'grey50'),
+    plot.caption.position = 'plot')
+
+# Legend 
+legend <- data.table(level = c(NA, sort(z[,unique(bin)])),
+                 xmin = c(0:6),
+                 xmax = c(1:7), 
+                 label = c('Missing', '<0.001%', '0.001 - 0.01%', '0.01 - 0.1%',
+                           '0.1 - 1%', '1 - 10%', '>10%'),
+                 loc = c(-0.5, 1.5, -0.5, 1.5, -0.5, 1.5, -0.5))
+
+
+leg <- ggplot(legend) +
+  geom_rect(
+    aes(xmin = xmin, xmax = xmax, ymin = 0, ymax = 1, fill = level), 
+    color = 'grey10',
+    size = 0.15) +
+  geom_text(
+    aes(y = loc, x = xmin + 0.5, label = label),
+    vjust = 'middle',
+    hjust = 'middle',
+    size = 3,
+    colour = "black",
+    family = "Bebas")+
+  scale_fill_manual(values = pal, na.value = '#4B3B40')+
+  scale_y_continuous(limits = c(-2,2))+
+  guides(fill = FALSE) +
+  theme_void() + 
+  theme(plot.margin = margin(c(2, 2 , 2, 2)))+
+  coord_equal()
+
+
+# build plot 
+p + inset_element(
+  leg,
+  left = 0.75, 
+  bottom = 0.85,
+  right = 1,
+  top = 1,
+  clip = FALSE,
+  align_to = "full") +
+  ggsave(
+    filename = paste0(getwd(), '/plots/', tidy.week, '.png'),
+    width = 16, height = 10.5, device = 'png')
+
+
+
+
+
+
+
