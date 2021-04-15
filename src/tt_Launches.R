b776e57696bab31c8091254cae758ca1582abcca
josephedwardoreilly <jo13931@bristol.ac.uk>
2021-04-15 13:13:41 +0100

    Final code and plot

--- src/tt_Launches.R
+++ src/tt_Launches.R
@@ -2,23 +2,19 @@ library(tidytuesdayR)
 library(ggplot2)
 library(data.table)
 library(ggtext)
-library(geofacet)
 library(patchwork)
 
 # Data Prep ---------------------------------------------------------------
 # Read the data
 tidy.week <- '2019-01-15'
-
 tt_data <- tt_load(tidy.week) 
-
 x <- data.table(tt_data$launches)
+x <- x[launch_date < '2020-01-01'] # really simple clean
 
 
-x <- x[launch_date < '2020-01-01']
-x[state_code == 'SU', state_code := 'RU']
 
+# Plotting ----------------------------------------------------------------
 bg <- '#242424'
-bg.2 <- '#242424'
 
 pal <- c(
   'state' = '#7EBDC2',
@@ -26,27 +22,53 @@ pal <- c(
   'private' = '#BB4430'
 )
 
+# Main text
+body.text <- 'Non-State actors are playing an increasing role in rocket launches since the late 1980s. Each point represents a single rocket launch and is coloured based on the type of agency performing the launch. Launches by state based agencies are slowly being replaced by ones conducted by private companies and startups.'
 
-# Plot the title and body text 
+body.text <- gsub(
+  x = body.text,
+  pattern = 'state',
+  replacement = paste0(
+    "<span style = 'color:",
+    pal['state'],
+    ";'>**state**</span>"))
+
+body.text <- gsub(
+  x = body.text,
+  pattern = 'private',
+  replacement = paste0(
+    "<span style = 'color:",
+    pal['private'],
+    ";'>**private**</span>"))
+
+body.text <- gsub(
+  x = body.text,
+  pattern = 'startups',
+  replacement = paste0(
+    "<span style = 'color:",
+    pal['startup'],
+    ";'>**startups**</span>"))
+
+# Plot the body text 
 pt.body <- ggplot() + 
   # Text body
   geom_textbox(
     data = data.frame(
       x = 0.1,
       y = 0.80,
-      label = 'The distribution of the lightness of foundation shades listed on Sephora and Ulta is not uniform. Plot (a) shows the colour distribution of foundation shades from the 10 largest brands, as measured by the number of unique products the brand produces.'),
+      label = body.text),
   aes(x, y, label = label),
-  color = 'grey90',
+  color = 'grey70',
   box.color = NA,
-  family = 'Raleway',
+  family = 'Antonio Light',
   fill = NA,
-  size = 3.5,
-  width = grid::unit(0.5, "npc"), halign = 0.5) + 
+  size = 4,
+  width = grid::unit(0.6, "npc"), halign = 0.5) + 
   theme_void()
 
       
-
-p.main <- ggplot(x, aes(x = launch_date, y = 1, color = agency_type, fill = agency_type)) + 
+# Plot the data
+p.main <- ggplot(x, aes(x = launch_date, y = 1, color = agency_type, fill = agency_type)) +
   geom_jitter(alpha = 0.75, height = 0.2, size = .75) + 
   theme_void() + 
   scale_x_date(
@@ -54,31 +76,40 @@ p.main <- ggplot(x, aes(x = launch_date, y = 1, color = agency_type, fill = agen
     expand = c(0,0),
     breaks = as.Date(c('1960-01-01', '1980-01-01', '2000-01-01', '2019-01-01')),
     date_labels = '%Y') + 
-  scale_y_continuous(limits = c(0.3, 1.2))+
+  scale_y_continuous(
+    limits = c(0.3, 1.2))+
   coord_polar() +
-  labs(title = 'Space Travel - A Private Enterprise', 
-       caption = "Visualisation by Joe O'Reilly (josephedwardoreilly@github.com)") +
+  labs(title = 'Space - An Increasingly Private Enterprise', 
+       caption = "Visualisation by Joe O'Reilly (josephedwardoreilly@github.com)\nData from TidyTuesday and planet4589.org/space/lvdb/index.html") +
   scale_color_manual(values = pal) +
-  theme(legend.position = 'none',
-        panel.background = element_rect(color = NA, fill = bg),
-        panel.spacing = unit(.5, "lines"),
-        axis.text.x = element_text(color = 'grey90'),
-        text = element_text('Raleway'),
-        plot.background = element_rect(fill = bg.2, color = bg.2),
-        plot.margin = margin(c(20, 40 , 20, 40)),
-        plot.caption = element_text(family = 'Raleway', colour = 'grey90'),
-        plot.title = element_text(
-          family = 'Raleway',
-          colour = 'grey90',
-          hjust = 0.5,
-          size = 25,
-          margin = margin(20, 20, 10, 20)))
+  theme(
+    legend.position = 'none',
+    panel.background = element_rect(color = NA, fill = bg),
+    panel.spacing = unit(.5, "lines"),
+    axis.text.x = element_text(color = 'grey70'),
+    text = element_text('Antonio'),
+    plot.background = element_rect(fill = bg, color = bg),
+    plot.margin = margin(c(20, 40 , 20, 40)),
+    plot.caption = element_text(
+      family = 'Antonio Light',
+      colour = 'grey50'),
+    plot.title = element_text(
+      family = 'Antonio',
+      colour = 'grey90',
+      hjust = 0.5,
+      size = 30,
+      margin = margin(20, 20, 10, 20)))
 
-
-p.main + inset_element(
-  pt.body,
-  left = 0.25, 
-  bottom = 0.25,
-  right = 0.75,
-  top = 0.75)
+# Build final structure
+p.main + 
+  inset_element(
+    pt.body,
+    left = 0.25, 
+    bottom = 0.25,
+    right = 0.75,
+    top = 0.75) + 
+  ggsave(
+    filename = paste0(getwd(), '/plots/', tidy.week, '.png'),
+    width = 10.5, height = 10.5, device = 'png'
+  )
 
