31b1b272b5debd0090aeb3c0c6dd3c6d8173813f
josephedwardoreilly <jo13931@bristol.ac.uk>
2021-05-24 18:18:23 +0100

    Added final plots and code for managers

--- src/tt_Manager.R
+++ src/tt_Manager.R
@@ -159,6 +159,42 @@ jk <- rbindlist(jk, idcol = 'edu')
 # Define a palette
 man.pal <- c('Man' = '#247BA0', 'Woman' = '#F49D6E')
 
+# Build a formatted subtitle text 
+sub.text <- 'This visualisation presents the results from three Bayesian hierarchical regrssion models used to estimate the association between gender and annual compensation (i.e. salary + bonus) as reported <br> by respondents of a survey by AskAManager.org. The model acheives partial pooling through the inclusion of random intercepts, clustered by industry. <br> The top row shows the posterior distribution (pp > 0.5, 0.8, 0.95), of the expected increase in annual compensation given a single year increase in field specific experience. The bottom row presents <br> posterior samples of the estimated association between gender, experience, and annual compensation. Males are presented in blue, and females are presented in orange.<br><br> Males with college or postgraduate degrees earn more than females and experience greater wage growth with each year of field experience. <br> This relationship is not clearly observed for individuals with a professional degree, although more data is required to investigate this thoroughly.'
+
+sub.text <- gsub(
+  x = sub.text,
+  pattern = 'Males',
+  replacement = paste0(
+    "<span style = 'color:",
+    man.pal['Man'],
+    ";'>**MALES**</span>"))
+
+sub.text <- gsub(
+  x = sub.text,
+  pattern = 'blue',
+  replacement = paste0(
+    "<span style = 'color:",
+    man.pal['Man'],
+    ";'>**BLUE**</span>"))
+
+sub.text <- gsub(
+  x = sub.text,
+  pattern = 'females',
+  replacement = paste0(
+    "<span style = 'color:",
+    man.pal['Woman'],
+    ";'>**FEMALES**</span>"))
+
+sub.text <- gsub(
+  x = sub.text,
+  pattern = 'orange',
+  replacement = paste0(
+    "<span style = 'color:",
+    man.pal['Woman'],
+    ";'>**ORANGE**</span>"))
+
+
 # Sampled linear relationships
 p.lin <- ggplot(
   k,
@@ -173,45 +209,79 @@ p.lin <- ggplot(
   xlab('Years Of Field Experience') + 
   ylab('Total Annual Compensation ($)') + 
   theme(
-    text = element_text(family = 'Gilroy-Light', color = '#08090A'),
-    plot.background = element_rect(fill = '#FFFFFF', color = NA),
+    text = element_text(
+      family = 'Gilroy-Light', 
+      color = '#08090A'),
+    plot.background = element_rect(
+      fill = '#FFFFFF', 
+      color = NA),
     panel.grid = element_blank(),
-    panel.background = element_rect(fill = '#F7F7FF', color = NA),
-    strip.background = element_rect(fill = '#F7F7FF', color = NA),
-    strip.text = element_text(hjust = 1,  colour = '#08090A', family = 'Gilroy-SemiBold')) + 
+    panel.background = element_rect(
+      fill = '#F7F7FF', 
+      color = NA),
+    strip.background = element_rect(
+      fill = '#F7F7FF',
+      color = NA),
+    strip.text = element_text(
+      hjust = 1, 
+      colour = '#08090A',
+      family = 'Gilroy-SemiBold')) + 
   guides(colour = FALSE)
 
 # Sampled interaction effect
 p.int <- ggplot(
     jk,
     aes(x = factor(gender), y = value, color = gender)) + 
-    stat_interval(aes(color_ramp = stat(level))) + 
+    stat_interval(
+      aes(color_ramp = stat(level)),
+      size  = 15) + 
     facet_wrap(.~edu) + 
     xlab(NULL) + 
     ylab('Growth In Annual Compensation \n Per-Year Of Field Experience ($)') + 
     scale_color_manual(values = man.pal) + 
     guides(color = FALSE, color_ramp = FALSE) + 
+    labs(
+      title = 'BAYESIAN HIERARCHICAL MODELLING OF GENDER PAY DISPARITY', 
+      subtitle = sub.text) + 
   theme(
-    text = element_text(family = 'Gilroy-Light', color = '#08090A'),
+    text = element_text(
+      family = 'Gilroy-Light',
+      color = '#08090A'),
+    plot.title = element_text(
+      family = 'Gilroy-Bold',
+      size = 12,
+      color = '#08090A'),
+    plot.subtitle = element_markdown(
+      family = 'Gilroy-Regular',
+      size = 7,
+      color = '#08090A'),
     axis.text.x = element_blank(),
     axis.ticks.x = element_blank(),
     axis.line.x = element_blank(),
     panel.grid = element_blank(),
-    plot.background = element_rect(fill = '#FFFFFF', color = NA),
-    panel.background = element_rect(fill = '#F7F7FF', color = NA),
-    strip.background = element_rect(fill = '#F7F7FF', color = NA),
-    strip.text = element_text(hjust = 1, colour = '#08090A', family = 'Gilroy-SemiBold'))
+    plot.background = element_rect(
+      fill = '#FFFFFF',
+      color = NA),
+    panel.background = element_rect(
+      fill = '#F7F7FF',
+      color = NA),
+    strip.background = element_rect(
+      fill = '#F7F7FF',
+      color = NA),
+    strip.text = element_text(
+      hjust = 1, 
+      colour = '#08090A',
+      family = 'Gilroy-SemiBold'))
 
 # Final plot construction 
-# TODO:  add a title to the top plot and a subtitle explaining the content
 (p.int/p.lin) + 
   plot_annotation(
-    caption = "Visualisation by Joe O'Reilly (josephedwardoreilly.github.com)\nData from TidyTuesday and AskAManager",
+    caption = "Visualisation by Joe O'Reilly (josephedwardoreilly.github.com)\nData from TidyTuesday and AskAManager.org",
     theme = theme(
       plot.margin = margin(5, 0, 10, 0),
       plot.caption = element_text(
         family = 'Gilroy-Regular',
-        size = 8,
+        size = 6,
         colour = '#08090A'),
       plot.background = element_rect(
         fill = '#FFFFFF',
@@ -222,5 +292,7 @@ p.int <- ggplot(
       '/plots/',
       tidy.week,
       '.png'),
-    width = 11, height = 7, device = 'png')
+    width = 11,
+    height = 7,
+    device = 'png')
 
