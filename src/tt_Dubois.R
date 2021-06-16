a31ff4fcd3134784123b87abc7f95897ce222d09
josephedwardoreilly <jo13931@bristol.ac.uk>
2021-06-16 17:03:17 +0100

    Plot update

--- src/tt_Dubois.R
+++ src/tt_Dubois.R
@@ -12,7 +12,21 @@ x <- data.table(tt_data$tweets)
 
 # Keep data from the week of the challenge only
 x <- x[format(datetime, '%Y-%m-%d') %between% c('2021-02-16', '2021-02-23')]
-x[, hour:=format(datetime, '%H')]
+# Extract hour, minute, and hour + minute as integers
+x[, hour := as.integer(format(datetime, '%H'))]
+x[, minute := as.integer(format(datetime, '%M'))]
+x[, hm := hour + (minute/60)]
+
+# add unique identifier 
+x[,UID := 1:nrow(x)]
+
+# unique tweets 
+zz <- merge(
+  melt(x[,.(UID, LIKES = like_count, RETWEETS = retweet_count)], id.vars = 'UID'),
+  x[,.(UID, hm)],
+  by = 'UID')
+
+
 
 # Summary of engagement
 z <- x[!is.na(hour), .(
@@ -39,30 +53,38 @@ z[, value := value/N]
 #     by = 'tweet')
 
 
-
-
 # Plotting ----------------------------------------------------------------
 
 # For formatting
 z[, variable := toupper(variable)]
-pal <- c('#88A096', '#ECA72C')
+pal <- c('#AB3428', '#ECA72C')
 
 ggplot(z[variable != 'N'],
        aes(
          x = hour,
          y = value,
-         size = value,
          group = hour,
          color = variable)) + 
-  geom_point(alpha = 0.95) + 
+  geom_point(
+    data = zz,
+    inherit.aes = FALSE,
+    color = 'grey',
+    alpha = 0.1,
+    aes(
+      x = hm,
+      y = value)) + 
+  geom_point(
+    alpha = 0.95,
+    size = 2) + 
   facet_wrap(.~variable, scales = 'free', nrow = 3)  +
   guides(color = FALSE, size = FALSE) + 
   theme_void() + 
   labs(title = 'TidyTuesday Twitter Traction',
-       subtitle = 'On average, how many engagements per-tweet did a TidyTuesday tweet gain at different times of the day during the Du Bois challenge?',
+       subtitle = 'On average, how many engagements per-tweet did a TidyTuesday tweet gain at different times of the day during the Du Bois challenge?<br>The hourly average is shown in colour and the engagements for each individual tweet are shown by the grey points.',
        caption = "Visualisation by Joe O'Reilly (github.com/josephedwardoreilly) - Data from TidyTuesday and #DuBoisChallenge") + 
   scale_color_manual(values = pal) + 
   scale_y_continuous(expand = c(.1,0), limits = c(0, NA)) + 
+  scale_x_continuous(limits = c(0, 24), expand = c(.01, .01)) + 
   theme(
     text = element_text(family = 'Apercu Pro', color = 'white'),
     plot.title = element_markdown(
